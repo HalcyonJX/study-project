890a2e2bc5da2c6ae466bbc5ced892dd70190e53
HalcyonJX <502229027@qq.com>
2024-05-27 16:45:56 +0800

    route
    添加路由

--- study-project-frontend/src/App.vue
+++ study-project-frontend/src/App.vue
@@ -1,53 +1,9 @@
 <script setup>
-import {User,Lock} from '@element-plus/icons-vue'
+
 </script>
 
 <template>
-  <div style="width: 100vw;height: 100vh;overflow: hidden;display: flex">
-    <div style="flex: 1;background-color: dodgerblue">
-      <el-image style="width: 100%;height: 100%" fit="cover"
-      src="https://raw.githubusercontent.com/HalcyonJX/blog-img/main/blog-imgAnime-Girl-on-the-Sky-Bridge.jpg"/>
-    </div>
-    <div style="width: 400px;background-color: white">
-      <div style="text-align: center;margin:0 20px">
-        <div style="margin-top: 150px">
-          <div style="font-size: 25px;font-weight: bold">登录</div>
-          <div style="font-size: 14px;color: grey">在进入系统之前请先输入用户名和密码</div>
-        </div>
-        <div style="margin-top: 50px">
-          <el-input type="text" placeholder="用户名/邮箱">
-            <template #prefix>
-              <el-icon><User /></el-icon>
-            </template>
-          </el-input>
-          <el-input type="password" style="margin-top: 10px" placeholder="密码">
-            <template #prefix>
-              <el-icon><Lock /></el-icon>
-            </template>
-          </el-input>
-        </div>
-          <el-row style="margin-top: 5px">
-            <el-col :span="12" style="text-align: left">
-              <el-checkbox v-model="checked1" label="记住我"/>
-            </el-col>
-            <el-col :span="12" style="text-align: right">
-              <el-link >忘记密码</el-link>
-            </el-col>
-          </el-row>
-        <div>
-          <div style="margin-top: 30px">
-            <el-button style="width: 270px" type="success" plain>立即登录</el-button>
-          </div>
-          <el-divider>
-            <span style="color: grey;font-size: 13px">没有账号</span>
-          </el-divider>
-          <div>
-            <el-button style="width: 270px" type="warning" plain>注册账号</el-button>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <router-view/>
 </template>
 
 <style scoped>
---
+++ study-project-frontend/src/views/Welcome.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+</script>
+
+<template>
+  <div style="width: 100vw;height: 100vh;overflow: hidden;display: flex">
+    <div style="flex: 1;background-color: dodgerblue">
+      <el-image style="width: 100%;height: 100%" fit="cover"
+                src="https://raw.githubusercontent.com/HalcyonJX/blog-img/main/blog-imgAnime-Girl-on-the-Sky-Bridge.jpg"/>
+    </div>
+    <div class="welcome-title">
+      <div style="font-size: 30px;font-weight: bold">欢迎来到我们的学习平台</div>
+      <div style="margin-top: 10px">在这里你可以学习到如何使用Java，如何搭建网站</div>
+    </div>
+    <div style="width: 400px;background-color: white">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.welcome-title{
+  position: absolute;
+  bottom: 30px;
+  left: 30px;
+  color: white;
+  text-shadow: 0 0 10px black;
+}
+</style>
